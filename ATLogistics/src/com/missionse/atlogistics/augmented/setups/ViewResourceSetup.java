141c83603fec0306777c4b04ffe21a85accc5bd6
jsantangelo <jamesfsantangelo@gmail.com>
2013-11-13 14:39:23 -0500

    Fixed stupid mistake.

--- ATLogistics/src/com/missionse/atlogistics/augmented/setups/ViewResourceSetup.java
+++ ATLogistics/src/com/missionse/atlogistics/augmented/setups/ViewResourceSetup.java
@@ -23,58 +23,51 @@ import com.missionse.atlogistics.resources.ResourceManager;
 
 public class ViewResourceSetup extends DefaultMultiSetup {
 
-	public ViewResourceSetup(Activity parent) {
+	public ViewResourceSetup(final Activity parent) {
 		super(parent, false);
-		
-		
+
 	}
 
 	@Override
-	public void _a4_addDefaultMarkers(MarkerObjectMap markerObjectMap) {
+	public void _a4_addDefaultMarkers(final MarkerObjectMap markerObjectMap) {
 		// TODO Auto-generated method stub
 
 	}
 
 	@Override
-	public void _b2_addDefaultWorldObjects(World world) {
+	public void _b2_addDefaultWorldObjects(final World world) {
 		List<Resource> resources = ResourceManager.getInstance().getResources();
-		
-		for(Resource r : resources){
-			MeshComponent m = GLFactory.getInstance().newTexturedSquare(
-					r.getResourceName(),
-					IO.loadBitmapFromId(
-							getActivity(), 
-							r.getType().getResourceId()));
-			
-			m.addChild(new AnimationFaceToCamera(getCamera(),0.5f));
-			m.setScale(new Vec(3,3,3));
-			
+
+		for (Resource r : resources) {
+			MeshComponent m = GLFactory.getInstance().newTexturedSquare(r.getResourceName(),
+					IO.loadBitmapFromId(getActivity(), r.getType().getResourceId()));
+
+			m.addChild(new AnimationFaceToCamera(getCamera(), 0.5f));
+			m.setScale(new Vec(3, 3, 3));
+
 			TextView v = new TextView(getActivity());
 			v.setTypeface(null, Typeface.BOLD);
 			StringBuilder sb = new StringBuilder();
 			sb.append(r.getResourceName() + ": ");
-			sb.append("(" + r.getCount() + "/" + r.getFillCount() + ")");
+			//sb.append("(" + r.getCount() + "/" + r.getFillCount() + ")");
 			v.setText(sb.toString());
 
 			Random ran = new Random();
-			
-			MeshComponent mesh = GLFactory.getInstance().newTexturedSquare(
-					"txtView" + r.getResourceName() + r.getCount() + r.getFillCount() + ran.nextInt(100) , 
-					util.IO.loadBitmapFromView(v), 
-					2);
-			mesh.addAnimation(new AnimationFaceToCamera(getCamera()));
-			
+
+			//MeshComponent mesh = GLFactory.getInstance().newTexturedSquare(
+			//		"txtView" + r.getResourceName() + r.getCount() + r.getFillCount() + ran.nextInt(100) , 
+			//		util.IO.loadBitmapFromView(v), 
+			//		2);
+			//mesh.addAnimation(new AnimationFaceToCamera(getCamera()));
+
 			final MeshComponent itemMesh = new Shape();
 			itemMesh.addChild(m);
-			itemMesh.addChild(mesh);
+			//itemMesh.addChild(mesh);
 			itemMesh.addAnimation(new AnimationFaceToCamera(getCamera()));
-			
+
 			Obj o = new Obj();
 			o.setComp(itemMesh);
-			o.setPosition(Vec.getNewRandomPosInXYPlane(
-					getCamera().getPosition(), 
-					10, 
-					40));
+			o.setPosition(Vec.getNewRandomPosInXYPlane(getCamera().getPosition(), 10, 40));
 			//o.setMyLatitude(Vec.);
 			//o.setMyLongitude(r.getLon());
 			//o.setMyAltitude(getCamera().getGPSPositionAsGeoObj().getAltitude());
@@ -84,7 +77,7 @@ public class ViewResourceSetup extends DefaultMultiSetup {
 	}
 
 	@Override
-	public void _e3_addElementsToUi(GuiSetup guiSetup, Activity activity) {
+	public void _e3_addElementsToUi(final GuiSetup guiSetup, final Activity activity) {
 		// TODO Auto-generated method stub
 
 	}
